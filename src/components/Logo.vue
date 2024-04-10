fb4e58f93f39e759c0f3a086f3a7dfa9b8291b38
Leopholdo Ishibashi <shideki13@gmail.com>
2024-04-10 16:04:49 -0300

    feat: Atualiza a logo e favicons do sistema;

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="d-flex align-center" :class="{'sm': props.sm}">
-    <div class="kanji">
+    <!-- <div class="kanji">
       流れ
-    </div>
+    </div> -->
+    <img class="logo-img" src="@/assets/logo.webp"/>
     <span>
       Nagare
     </span>
@@ -17,6 +18,11 @@ const props = defineProps([
 </script>
 
 <style lang="scss" scoped>
+  .logo-img {
+    width: 50px;
+    margin-right: 5px;
+  }
+
   .kanji {
     writing-mode: vertical-lr;
     font-size: 1.2rem;
@@ -31,9 +37,14 @@ const props = defineProps([
   }
 
   .sm {
+    .logo-img {
+      width: 40px !important;
+    }
+
     .kanji {
       font-size: 1rem !important;
     }
+
     span {
       font-size: 2rem !important;
     }
